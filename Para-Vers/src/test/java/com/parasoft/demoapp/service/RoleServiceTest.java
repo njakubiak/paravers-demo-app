c0ba140cb3cecc490ae6751599f70fe46e48608f
Nathan Jakubiak <jakubiak@parasoft.com>
2020-12-14 16:52:54 -0800

    Added RoleServiceTest.

---
+++ Para-Vers/src/test/java/com/parasoft/demoapp/service/RoleServiceTest.java
@@ -0,0 +1,137 @@
+/**
+ * 
+ */
+package com.parasoft.demoapp.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.parasoft.demoapp.exception.RoleNameExistsAlreadyException;
+import com.parasoft.demoapp.exception.RoleNotFoundException;
+import com.parasoft.demoapp.model.global.RoleEntity;
+import com.parasoft.demoapp.repository.global.RoleRepository;
+
+/**
+ * Parasoft Jtest UTA: Test class for RoleService
+ *
+ * @see com.parasoft.demoapp.service.RoleService
+ * @author jakubiak
+ */
+public class RoleServiceTest {
+
+	/**
+	 * Parasoft Jtest UTA: Test for addNewRole(String)
+	 *
+	 * @see com.parasoft.demoapp.service.RoleService#addNewRole(String)
+	 * @author jakubiak
+	 */
+	@Test
+	public void testAddNewRole() throws Throwable {
+		// Given
+		RoleService underTest = new RoleService();
+		RoleRepository roleRepositoryValue = TestFactory.createRoleRepository();
+		ReflectionTestUtils.setField(underTest, "roleRepository", roleRepositoryValue);
+
+		// When
+		String roleName = "admin"; // UTA: default value
+		RoleEntity result = underTest.addNewRole(roleName);
+
+		// Then
+		assertNotNull(result);
+		assertEquals("Role: admin", result.getName());
+	}
+
+	/**
+	 * Parasoft Jtest UTA: Test for getRoleByRoleName(String)
+	 *
+	 * @see com.parasoft.demoapp.service.RoleService#getRoleByRoleName(String)
+	 * @author jakubiak
+	 */
+	@Test
+	public void testGetRoleByRoleName() throws Throwable {
+		// Given
+		String roleName = "admin";
+		RoleService underTest = new RoleService();
+		RoleRepository roleRepositoryValue = TestFactory.createRoleRepository();
+		roleRepositoryValue.save(new RoleEntity(roleName));
+		ReflectionTestUtils.setField(underTest, "roleRepository", roleRepositoryValue);
+
+		// When
+		RoleEntity result = underTest.getRoleByRoleName(roleName);
+
+		// Then
+		assertNotNull(result);
+		assertEquals(roleName, result.getName());
+	}
+
+	/**
+	 * Parasoft Jtest UTA: Test for roleExists(String)
+	 *
+	 * @see com.parasoft.demoapp.service.RoleService#roleExists(String)
+	 * @author jakubiak
+	 */
+	@Test
+	public void testRoleExists() throws Throwable {
+		// Given
+		String roleName = "admin";
+		RoleService underTest = new RoleService();
+		RoleRepository roleRepositoryValue = TestFactory.createRoleRepository();
+		roleRepositoryValue.save(new RoleEntity(roleName));
+		ReflectionTestUtils.setField(underTest, "roleRepository", roleRepositoryValue);
+
+		// When
+		boolean result = underTest.roleExists(roleName);
+
+		// Then - assertions for result of method roleExists(String)
+		assertTrue(result);
+	}
+
+	/**
+	 * Parasoft Jtest UTA: Test for addNewRole(String)
+	 *
+	 * @see com.parasoft.demoapp.service.RoleService#addNewRole(String)
+	 * @author jakubiak
+	 */
+	@Test(expected = RoleNameExistsAlreadyException.class)
+	public void testAddNewRole2() throws Throwable {
+		// Given
+		String roleName = "admin";
+		RoleService underTest = new RoleService();
+		RoleRepository roleRepositoryValue = TestFactory.createRoleRepository();
+		roleRepositoryValue.save(new RoleEntity(roleName));
+		ReflectionTestUtils.setField(underTest, "roleRepository", roleRepositoryValue);
+
+		// When
+		underTest.addNewRole(roleName);
+	}
+
+	/**
+	 * Parasoft Jtest UTA: Test for getRoleByRoleName(String)
+	 *
+	 * @see com.parasoft.demoapp.service.RoleService#getRoleByRoleName(String)
+	 * @author jakubiak
+	 */
+	@Test(expected = RoleNotFoundException.class)
+	public void testGetRoleByRoleName2() throws Throwable {
+		// UTA: One or more of the required values have not been automatically set.
+		// Review the information about the required modifications below and manually update the missing value(s).
+		// 
+		// To cover the code, the 'this.roleRepository.findByName(java.lang.String)' method must return 'null'.
+
+		// Given
+		RoleService underTest = new RoleService();
+		RoleRepository roleRepositoryValue = TestFactory.createRoleRepository();
+		ReflectionTestUtils.setField(underTest, "roleRepository", roleRepositoryValue);
+
+		// When
+		String name = ""; // UTA: default value
+		RoleEntity result = underTest.getRoleByRoleName(name);
+
+		// Then
+		// assertNotNull(result);
+	}
+}
---
+++ Para-Vers/src/test/java/com/parasoft/demoapp/service/TestFactory.java
@@ -0,0 +1,157 @@
+package com.parasoft.demoapp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.parasoft.demoapp.model.global.RoleEntity;
+import com.parasoft.demoapp.repository.global.RoleRepository;
+
+public class TestFactory {
+	/**
+	 * @jtest.factory
+	 */
+	public static RoleRepository createRoleRepository() {
+		RoleRepository repo = new RoleRepository()
+			{
+				private RoleEntity roleEntity;
+					@Override
+					public List<RoleEntity> findAll() {
+						return null;
+					}
+
+					@Override
+					public List<RoleEntity> findAll(Sort sort) {
+						return null;
+					}
+
+					@Override
+					public List<RoleEntity> findAllById(Iterable<Long> ids) {
+						return null;
+					}
+
+					@Override
+					public <S extends RoleEntity> List<S> saveAll(Iterable<S> entities) {
+						return null;
+					}
+
+					@Override
+					public void flush() {
+					}
+
+					@Override
+					public <S extends RoleEntity> S saveAndFlush(S entity) {
+						return null;
+					}
+
+					@Override
+					public void deleteInBatch(Iterable<RoleEntity> entities) {
+					}
+
+					@Override
+					public void deleteAllInBatch() {
+					}
+
+					@Override
+					public RoleEntity getOne(Long id) {
+						return null;
+					}
+
+					@Override
+					public <S extends RoleEntity> List<S> findAll(Example<S> example) {
+						return null;
+					}
+
+					@Override
+					public <S extends RoleEntity> List<S> findAll(Example<S> example, Sort sort) {
+						return null;
+					}
+
+					@Override
+					public Page<RoleEntity> findAll(Pageable pageable) {
+						return null;
+					}
+
+					@Override
+					public RoleEntity save(RoleEntity entity) {
+						this.roleEntity = entity;
+						return entity;
+					}
+
+					@Override
+					public Optional<RoleEntity> findById(Long id) {
+						return null;
+					}
+
+					@Override
+					public boolean existsById(Long id) {
+						return false;
+					}
+
+					@Override
+					public long count() {
+						return 0;
+					}
+
+					@Override
+					public void deleteById(Long id) {
+						
+					}
+
+					@Override
+					public void delete(RoleEntity entity) {
+						
+					}
+
+					@Override
+					public void deleteAll(Iterable<? extends RoleEntity> entities) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void deleteAll() {
+						
+					}
+
+					@Override
+					public <S extends RoleEntity> Optional<S> findOne(Example<S> example) {
+						return null;
+					}
+
+					@Override
+					public <S extends RoleEntity> Page<S> findAll(Example<S> example, Pageable pageable) {
+						return null;
+					}
+
+					@Override
+					public <S extends RoleEntity> long count(Example<S> example) {
+						return 0;
+					}
+
+					@Override
+					public <S extends RoleEntity> boolean exists(Example<S> example) {
+						return false;
+					}
+
+					@Override
+					public boolean existsByName(String roleName) {
+						return roleEntity != null && roleEntity.getName().equals(roleName);
+					}
+
+					@Override
+					public RoleEntity findByName(String roleName) {
+						if (existsByName(roleName)) {
+							return roleEntity;
+						}
+						return null;
+					}
+			
+				};
+		return repo;
+	}
+}
